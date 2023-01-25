52acd3453855f08e2a74322cdfa89af4f28ada4f
ahroncich7 <alehroncich@gmail.com>
2023-01-25 14:06:23 -0300

    FIX: Elimina importación innecesaria

--- app/app.js
+++ app/app.js
@@ -1,20 +1,19 @@
-const { urlencoded } = require("express")
-const express = require("express")
-const app = express()
-const routes = require("./routes/routes")
-const cors = require("cors")
+const express = require("express");
+const app = express();
+const routes = require("./routes/routes");
+const cors = require("cors");
 
 // SETTINGS
 app.listen(8091, () => {
-    console.log("Server corriendo en puerto 3000")
-})
+	console.log("Server corriendo en puerto 3000");
+});
 
-app.use(express.static("../"))
-app.use(express.static("./"))
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(express.static("../"));
+app.use(express.static("./"));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 // app.use(cors())
 
 
 // ROUTES
-app.use("/", routes)
+app.use("/", routes);
