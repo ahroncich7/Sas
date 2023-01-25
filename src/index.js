a3767b640795b22ecaaac2fba7e061a05008efda
ahroncich7 <alehroncich@gmail.com>
2023-01-25 14:07:08 -0300

    FEAT: Agrega funciones de artCards

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import * as request from "./apiRequests.js"
+import { artCard } from "./artCard.js";
 import { updateProductsByType, updateSizesByType } from "./filter.js";
 import { productCard } from "./productCard.js";
 
 const productsInStockCont = document.getElementById("products-in-stock")
+const artCont = document.getElementById("art-cont")
 
 updateInStockProducts()
 updateSizesByType("Tazas")
 updateProductsByType("Tazas")
+updateArtCards()
 
 
 function updateInStockProducts() {
@@ -18,5 +21,19 @@ function updateInStockProducts() {
                 card.updateCard()
             }
         })
+    }).catch(e => console.log(e))
+}
+
+function updateArtCards() {
+    const productsPromise = request.getProductsByType("Arte")
+    productsPromise.then((productsList) => {
+        artCont.innerHTML = " "
+        productsList.data.forEach(productData => {
+            if (productData.stock) {
+                const card = new artCard(productData, artCont, "stock")
+                card.updateCard()
+            }
+        })
+        artCont.scrollLeft =  (artCont.scrollWidth - artCont.clientWidth ) / 2;
     }).catch(e => console.log(e))
 }
