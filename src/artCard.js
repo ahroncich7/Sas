5c3bb23fcd5af650df32f1fb8fdf1c625843e074
ahroncich7 <alehroncich@gmail.com>
2023-01-25 13:58:13 -0300

    FEAT: Crea clase para tarjetas de objetos de arte

---
+++ src/artCard.js
@@ -0,0 +1,22 @@
+import { productCard } from "./productCard.js";
+
+class artCard extends productCard {
+    constructor(data, productCardCont, cardType) {
+        super(data, productCardCont, cardType)
+
+        this.title, this.price, this.btn = ""
+
+    }
+
+    setCard() {
+        this.card =
+            `<div id = ${this.cardType}-${this.id} class="product-card art-card">
+                    ${this.img || undefined}
+                    <div class="product-card-desc">
+                        ${this.title || undefined}
+                    </div>
+            </div>`
+    }
+}
+
+export {artCard}
