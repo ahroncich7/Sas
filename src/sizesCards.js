061e556265848b8981a8534fb2cdae0923aaa68d
ahroncich7 <alehroncich@gmail.com>
2023-01-25 13:55:51 -0300

    CHANGE: Cambia h4 por h3 en sizesCards

--- src/sizesCards.js
+++ src/sizesCards.js
@@ -9,21 +9,21 @@ class sizesCard {
             `<div class="size-card small">
                 <img src="${data.size_img}" alt="${data.nombre_cat}">
                 <div class="size-desc">
-                    <h4>${data.small_cap}</h4>
+                    <h3>${data.small_cap} ml</h3>
                     <span>$${data.small_price}</span>
                 </div>
             </div>
             <div class="size-card medium">
                 <img src="${data.size_img}" alt="${data.nombre_cat}">
                 <div class="size-desc">
-                    <h4>${data.medium_cap}</h4>
+                    <h3>${data.medium_cap} ml</h3>
                     <span>$${data.medium_price}</span>
                 </div>
             </div>
             <div class="size-card large">
                 <img src="${data.size_img}" alt="${data.nombre_cat}">
                 <div class="size-desc">
-                    <h4>${data.large_cap}</h4>
+                    <h3>${data.large_cap} ml</h3>
                     <span>$${data.large_price}</span>
                 </div>
             </div>`
