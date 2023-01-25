f90ae828aa1d653aec6080b149d87ef359945d45
ahroncich7 <alehroncich@gmail.com>
2023-01-25 14:15:20 -0300

    FIX: Arregla identación acorde a ESLINT

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,36 +1,36 @@
-const express = require("express")
-const routes = express.Router()
-const productsController = require("../controllers/productsController.js")
+const express = require("express");
+const routes = express.Router();
+const productsController = require("../controllers/productsController.js");
 
 routes.get("/listByType", (req, res) => {
-    productsController.listByType(req, res)
-})
+	productsController.listByType(req, res);
+});
 
 routes.get("/listCategory", (req, res) => {
-    productsController.getCategory(req, res)
-})
+	productsController.getCategory(req, res);
+});
 
 routes.post("/insertProduct", (req, res) => {
-    productsController.insertNewProduct(req, res)
-})
+	productsController.insertNewProduct(req, res);
+});
 
 routes.get("/listFilteredProducts", (req, res) => {
-})
+});
 
 routes.delete("/deleteProduct", (req, res) => {
-    productsController.deleteProduct(req, res)
-})
+	productsController.deleteProduct(req, res);
+});
 
 routes.get("/listAll", (req, res) => {
-    productsController.listAll(req, res)
-})
+	productsController.listAll(req, res);
+});
 
 routes.get("/listInStock", (req, res) => {
-    productsController.listProductsInStock(req, res)
-})
+	productsController.listProductsInStock(req, res);
+});
 
 routes.put("/updateProduct", (req, res) => {
-        productsController.updateProduct(req, res)
-    })
+	productsController.updateProduct(req, res);
+});
 
-module.exports = routes
+module.exports = routes;
