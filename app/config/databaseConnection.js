7e7b2d55d395ec746dccc005566aaa1061ff40a8
ahroncich7 <alehroncich@gmail.com>
2023-01-25 13:58:56 -0300

    FIX: Identación acorde a ESLINT

--- app/config/databaseConnection.js
+++ app/config/databaseConnection.js
@@ -1,4 +1,4 @@
-const mysql = require('mysql');
+const mysql = require("mysql");
 // const DBCONFIG = {
 //     host: 'db4free.net',
 //     user: 'sasdbuser',
@@ -7,11 +7,11 @@ const mysql = require('mysql');
 // }
 
 const DBCONFIG = {
-    host: 'bxdraymchrkn0xyjbvgs-mysql.services.clever-cloud.com',
-    user: 'u02ffgz5temaryfh',
-    password: '03pZkHz2WCnVzrdf9iD2',
-    database: 'bxdraymchrkn0xyjbvgs'
-}
+	host: "bxdraymchrkn0xyjbvgs-mysql.services.clever-cloud.com",
+	user: "u02ffgz5temaryfh",
+	password: "03pZkHz2WCnVzrdf9iD2",
+	database: "bxdraymchrkn0xyjbvgs"
+};
 
 // const DBCONFIG = {
 //     host: '127.0.0.1',
@@ -22,51 +22,51 @@ const DBCONFIG = {
 
 class dbConnection {
 
-    constructor() {
-        try {
-            this.connection = mysql.createPool(
-                DBCONFIG
-            )
-            this.connection.query("SHOW DATABASES", (error, results) => {
-                if (!error) {
-                    console.log("Base de datos funcionando")
-                } else {
-                    console.log("No se pudo conectar la base de datos: ", error.message)
-                }
-            })
+	constructor() {
+		try {
+			this.connection = mysql.createPool(
+				DBCONFIG
+			);
+			this.connection.query("SHOW DATABASES", (error, results) => {
+				if (!error) {
+					console.log("Base de datos funcionando");
+				} else {
+					console.log("No se pudo conectar la base de datos: ", error.message);
+				}
+			});
 
-        } catch (e) {
-            console.log("No se pudo crear conexion con base de datos", e)
-        }
+		} catch (e) {
+			console.log("No se pudo crear conexion con base de datos", e);
+		}
 
-    }
+	}
 
-    query(query) {
-        // console.log("La query es :", query)
-        let response = {
-            status: "Fail",
-            data: ""
-        }
+	query(query) {
+		// console.log("La query es :", query)
+		let response = {
+			status: "Fail",
+			data: ""
+		};
 
-        if (this.connection) {
-            return new Promise((resolve, rejection) => {
-                this.connection.query(query, (error, results) => {
-                    if (!error && results != {}) {
-                        response = {
-                            status: "OK",
-                            data: results
-                        }
-                    } else {
-                        response.data = error
-                    }
-                    resolve(response)
-                })
-            })
-        } else {
-            response.data = "Base de datos desconectada"
-            return new Promise((resolve, rej) => resolve(response))
-        }
-    }
+		if (this.connection) {
+			return new Promise((resolve, rejection) => {
+				this.connection.query(query, (error, results) => {
+					if (!error && results != {}) {
+						response = {
+							status: "OK",
+							data: results
+						};
+					} else {
+						response.data = error;
+					}
+					resolve(response);
+				});
+			});
+		} else {
+			response.data = "Base de datos desconectada";
+			return new Promise((resolve, rej) => resolve(response));
+		}
+	}
 }
 
-module.exports = dbConnection
+module.exports = dbConnection;
