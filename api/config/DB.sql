a3bc7692e93e19a5f9d571897b0ed14e2b2d6cbd
ahroncich7 <alehroncich@gmail.com>
2023-02-08 16:42:41 -0300

    FEAT: Agrega ejemplos de inserción de datos en tabla categoría

--- api/config/DB.sql
+++ api/config/DB.sql
@@ -16,6 +16,8 @@ CREATE TABLE `categoria` (
 ) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8
 
 
+/*INSERT EXAMPLES*/ 
+
 CREATE TABLE `productos` (
  `titulo` varchar(30) DEFAULT NULL,
  `precio` int(10) DEFAULT NULL,
@@ -27,3 +29,112 @@ CREATE TABLE `productos` (
  KEY `cat_id2` (`cat_id2`),
  CONSTRAINT `productos_ibfk_1` FOREIGN KEY (`cat_id2`) REFERENCES `categoria` (`cat_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8
+
+
+INSERT INTO categoria VALUES (
+    null, 
+    "Tazas",
+    1000,
+    200,
+    "https://i.ibb.co/ZhQP7Wc/tazas.png",
+    1500,
+    250,
+    2000,
+    3000
+);
+
+INSERT INTO categoria VALUES (
+    null, 
+    "Cuencos",
+    1300,
+    200,
+    "https://i.ibb.co/pKRXFhj/cuencos.png",
+    1900,
+    250,
+    2200,
+    300
+);
+
+INSERT INTO categoria VALUES (
+    null, 
+    "Platos",
+    800,
+    200,
+    "https://i.ibb.co/ZxPhKgD/platos.png",
+    1200,
+    250,
+    2800,
+    300
+);
+
+INSERT INTO categoria VALUES (
+    null, 
+    "Bandejas",
+    1800,
+    300,
+    "https://i.ibb.co/7yWHrr9/bandejas.png",
+    2300,
+    360,
+    2900,
+    480
+);
+
+INSERT INTO categoria VALUES (
+    null, 
+    "Ensaladeras",
+    1000,
+    200,
+    "https://i.ibb.co/rtzf18Y/ensaladeras.png",
+    1500,
+    250,
+    2000,
+    300
+);
+
+INSERT INTO categoria VALUES (
+    null, 
+    "Floreros",
+    1000,
+    200,
+    "https://i.ibb.co/SRTdMRz/floreros.png",
+    1500,
+    250,
+    2000,
+    300
+);
+
+INSERT INTO categoria VALUES (
+    null, 
+    "Teteras",
+    1000,
+    200,
+    "https://i.ibb.co/tLD8mV7/teteras.png",
+    1500,
+    250,
+    2000,
+    300
+);
+
+INSERT INTO categoria VALUES (
+    null, 
+    "Objetos",
+    1000,
+    200,
+    "https://i.ibb.co/pxH8Wt0/objetos.png",
+    1500,
+    250,
+    2000,
+    300
+);
+
+INSERT INTO categoria VALUES (
+    null, 
+    "Arte",
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null
+);
