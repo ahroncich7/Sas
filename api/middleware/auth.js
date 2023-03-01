1587f1ee55c63a3866018d09ae7337031163cbde
ahroncich7 <alehroncich@gmail.com>
2023-03-01 13:44:24 -0300

    FIX: Arregla error en autenticador

--- api/middleware/auth.js
+++ api/middleware/auth.js
@@ -5,7 +5,8 @@ const authMiddleware = {
     autenticateAdmin: (req, res, next) => {
         
         // check if the req come from a client with valid admin pass 
-        if (req.query.pass & req.query.pass == ADMIN_PASS) {
+        if (req.query.pass && req.query.pass == ADMIN_PASS) {
+            console.log("acces");
             next();
         } else {
             res.redirect("/login");
