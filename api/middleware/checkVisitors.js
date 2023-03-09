9a882a0248301c3612cbde83d27d37d0bb5467c3
ahroncich7 <alehroncich@gmail.com>
2023-03-08 21:26:16 -0300

    FIX: Agrega next() al middleware

--- api/middleware/checkVisitors.js
+++ api/middleware/checkVisitors.js
@@ -1,11 +1,13 @@
 let petitionsCount = 0;
 
-function addVisit(){
+function addVisit(req, res, next){
     petitionsCount ++;
   
     if (petitionsCount % 3 === 0){
         console.log("Visitors", petitionsCount / 3);
     }
+
+    next();
 }
 
 module.exports = addVisit;
