50d01ca44858e80b8a2e8d78fdd692c20619a39c
ahroncich7 <alehroncich@gmail.com>
2023-03-12 21:48:13 -0300

    FEAT: Modifica el estilo para el boton de consulta

--- public/src/inStockProducts/view.js
+++ public/src/inStockProducts/view.js
@@ -11,18 +11,20 @@ export class product {
 
         this.card =
             `<div id = ${product.id} class="product-card">
-            <img id = img-${product.id} src="${product.img}" alt="${product.title}"></img>
-                    <div class="product-card-desc">
+
+                <div class="card-img">
+                    <img id = img-${product.id} src="${product.img}" alt="${product.title}"></img>
+                    <a href="${this.message}" target="_blank" class="call-to-action-small"><h2>CONSULTAR</h2></a>
+                </div>
+
+
+                <div class="product-card-desc">
                         
                     <h3 class="product-title">${product.title.toUpperCase()}</h3>
                         
-                        <span class="product-price">$${product.price}</span>
-                        
-                        <a href="${this.message}" target="_blank" class="call-to-action-small"><box-icon type='logo' name='whatsapp'></box-icon>CONSULTAR</a>
-
-
-
-                    </div>
+                    <span class="product-price">$${product.price}</span>
+                    
+                </div>
             </div>`;
 
         this.render();
