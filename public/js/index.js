05f5df078c311d81e7a3c735b32d3daf8d5be320
ahroncich7 <alehroncich@gmail.com>
2023-04-16 18:33:19 -0300

    FEAT: Redefine lógica del index para que cargue en segundo plano los datos

---
+++ public/js/index.js
@@ -0,0 +1,18 @@
+import { getProducts } from "../services/productsHandler.js";
+
+
+// Carga en segundo plano las peticiones a API.
+async function chargeProducts() {
+    let products = await getProducts();
+    let imgList = [];
+    products.data.forEach(product => {
+        imgList.push(product.img);
+    });
+    caches.open("imagenes").then(
+        (cache) => {
+            cache.addAll(imgList);
+        }
+    ).catch((e) => console.log(e));
+}
+
+chargeProducts();
