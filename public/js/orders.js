55cf62d2593ebfdfd44f041e0125f40b9d88e8ed
ahroncich7 <alehroncich@gmail.com>
2023-04-17 19:54:04 -0300

    FEAT: Comienza apartado Encargo

---
+++ public/js/orders.js
@@ -0,0 +1,14 @@
+
+//Carga datos de las medidas (sizes)
+const sizes = [];
+const sizesContainer = document.getElementById("carouselExample");
+
+//Limpia el contenedor del carrousel
+sizesContainer.innerHTML = "";
+
+//Itera las categorías e inyecta los fragmentos de HTML.
+sizes.forEach((cat)=>{
+    sizesContainer.innerHTML += `
+        
+    `
+});
