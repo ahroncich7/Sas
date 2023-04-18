6b829b1b9c402c68c3d0832c0a59adaf3aac1971
ahroncich7 <alehroncich@gmail.com>
2023-04-18 15:43:01 -0300

    FEAT: Prepara archivos para Encargos

--- public/js/orders.js
+++ public/js/orders.js
@@ -1,14 +1,14 @@
 
-//Carga datos de las medidas (sizes)
-const sizes = [];
-const sizesContainer = document.getElementById("carouselExample");
+// //Carga datos de las medidas (sizes)
+// const sizes = [];
+// const sizesContainer = document.getElementById("carouselExample");
 
-//Limpia el contenedor del carrousel
-sizesContainer.innerHTML = "";
+// //Limpia el contenedor del carrousel
+// sizesContainer.innerHTML = "";
 
-//Itera las categorías e inyecta los fragmentos de HTML.
-sizes.forEach((cat)=>{
-    sizesContainer.innerHTML += `
+// //Itera las categorías e inyecta los fragmentos de HTML.
+// sizes.forEach((cat)=>{
+//     sizesContainer.innerHTML += `
         
-    `
-});
+//     `
+// });
