fa61f85dbf5bfeb5dbcf06141d27b3a4945a3367
ahroncich7 <alehroncich@gmail.com>
2023-09-16 20:32:45 -0300

    FEAT: Define mensaje de contacto

--- public/components/footer/footer.js
+++ public/components/footer/footer.js
@@ -12,4 +12,17 @@ export async function setFooter() {
     const content = await html.text();
     footer.innerHTML = content;
 
+    document.getElementById("sendMessageBtn").addEventListener("click", () => {
+        let name = document.getElementById("name");
+        let email = document.getElementById("email");
+        let message = document.getElementById("message");
+        let messageText = message.value.replace(" ", "%20");
+        let whatsaapMessage = `https://wa.me/+543548434942?text=${messageText},nombre:%20${name.value}%20email:%20${email.value}`;
+        window.open(whatsaapMessage, "_blank");
+        name.value = "";
+        email.value = "";
+        message.value = "";
+    });
+
 }
+
